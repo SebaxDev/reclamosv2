b84c74b53761a3e54b8fd3abab0060452971fbad
SebaxDev <sebaxfusion@gmail.com>
2025-08-31 20:47:09 -0300

    Fix App

--- components/reclamos/__init__.py
+++ components/reclamos/__init__.py
@@ -1,1 +1,12 @@
-# Componentes modulares de la aplicación
+"""
+Paquete de componentes para la app de reclamos
+Permite importar submódulos como:
+    from components.auth import check_authentication
+"""
+
+# Importaciones opcionales para acceso directo
+from . import auth
+from . import navigation
+from . import metrics_dashboard
+from . import ui
+from . import resumen_jornada
